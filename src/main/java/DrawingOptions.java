6b932e33f7bffa10cb5cf3183b972ceef201c2e2
Briareus <bryan.freiermuth@students.fhnw.ch>
2015-05-26 15:31:36 +0200

    Merge branch 'master' of https://github.com/Fearghas/Projekt-Programmieren-II
    Conflicts:
    	src/main/java/MainPanel.java

---
+++ src/main/java/DrawingOptions.java
@@ -0,0 +1,126 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+
+/**
+ * Created by Briareus on 26.05.2015.
+ */
+public class DrawingOptions extends JPanel
+{
+    //Instanzen
+    private final Format datenmodell;
+    private String line;
+    private int pointSize;
+    double endpointx;
+    double endpointy;
+
+    public DrawingOptions(Format datenmodell)
+    {
+        this.datenmodell = datenmodell;
+    }
+
+    public int getPointSize()
+    {
+        return pointSize;
+    }
+
+    public void setPointSize(int pointSize)
+    {
+        this.pointSize = pointSize;
+    }
+
+    protected void paintComponent(Graphics g)
+    {
+        //Instanzen
+        ArrayList wertex = datenmodell.getarrayx();
+        ArrayList wertey = datenmodell.getarrayy();
+        double xMaximum = datenmodell.getMaximumx();
+        double xMinimum = datenmodell.getMinimumx();
+        double yMaximum = datenmodell.getMaximumy();
+        double yMinimum = datenmodell.getMinimumy();
+        double contentWidth;
+        double contentHeight;
+        String xlabel = datenmodell.getxName();
+        String ylabel = datenmodell.getyName();
+
+        //4 Bedingungen Koordinatensystem
+        if (xMinimum >= 0 && yMinimum >= 0)
+        {
+            contentWidth = xMaximum + xMinimum;
+            contentHeight = yMaximum + yMinimum;
+            xMinimum = 0;
+            yMinimum = 0;
+        }
+        else if (xMinimum < 1 && yMinimum >= 0)
+        {
+            xMinimum = -1 * xMinimum;
+            contentWidth = xMaximum + xMinimum;
+            contentHeight = yMaximum + yMinimum;
+            yMinimum = 0;
+        }
+        else if (xMinimum >= 0 && yMinimum < 1)
+        {
+            yMinimum = -1 * yMinimum;
+            contentWidth = xMaximum + xMinimum;
+            contentHeight = yMaximum + yMinimum;
+            xMinimum = 0;
+        }
+        else
+        {
+            xMinimum = -1 * xMinimum;
+            yMinimum = -1 * yMinimum;
+            contentWidth = xMaximum + xMinimum;
+            contentHeight = yMaximum + yMinimum;
+        }
+
+        final double scaleWidth = getWidth() / contentWidth;
+        final double scaleHeight = getHeight() / contentHeight;
+        Graphics2D g2d = (Graphics2D) g;
+        AffineTransform at = g2d.getTransform();
+        g2d.translate(0 + (scaleWidth) * xMinimum, getHeight() - (scaleHeight) * yMinimum);//Koordinatenursprung verschieben
+        g2d.scale(1, -1); //Invert the y-axis
+        setPointSize(5);
+        //drawPoints(wertex, wertey, g, scaleWidth, scaleHeight); //nur Punkte werden eingezeichnet
+        drawLines(wertex, wertey, g, scaleWidth, scaleHeight);//nur Linien werden eingezeichnet
+        //MenueBar();
+        g2d.setTransform(at);//y-axis wieder rückgängig gemacht, nicht notwendig
+    }
+
+    public void drawLines(ArrayList<Double> xWerte, ArrayList<Double> yWerte, Graphics g, double scaleWidth, double scaleHeight)
+    {
+        for (int i = 0; i < xWerte.size(); i++)
+        {
+            double getX = xWerte.get(i);
+            double getY = yWerte.get(i);
+            if (i == xWerte.size() - 1)//magic number
+            {
+                //endpointx = (Double) wertex.get(0);
+                //endpointy = (Double) wertey.get(0);
+            }
+            else
+            {
+                endpointx = xWerte.get(i + 1);
+                endpointy = yWerte.get(i + 1);
+            }
+            int x = (int) (getX * scaleWidth);
+            int y = (int) (getY * scaleHeight);
+            int nextpointx = (int) (endpointx * scaleWidth);
+            int nextpointy = (int) (endpointy * scaleHeight);
+            int correctionX = getPointSize() / 2;
+            int correctionY = getPointSize() / 2;
+            g.setColor(Color.red);
+            g.drawLine(x + correctionX, y + correctionY, nextpointx + correctionX, nextpointy + correctionY);
+        }
+    }
+
+    public int pointSize()
+    {
+        return 1;
+    }
+
+    public String colourPoints()
+    {
+        return null;
+    }
+}
