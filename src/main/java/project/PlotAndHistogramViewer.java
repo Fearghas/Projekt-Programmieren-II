f3a25750bab71a9e623c80905739f7d9a31f4bba
Briareus <bryan.freiermuth@students.fhnw.ch>
2015-06-07 14:03:21 +0200

    Immer ein Schritt näher zum Ziel.

---
+++ src/main/java/project/PlotAndHistogramViewer.java
@@ -0,0 +1,15 @@
+package project;
+import project.drawing.MainPanel;
+import javax.swing.*;
+
+public class PlotAndHistogramViewer extends JFrame {
+    public static void main(String[] args)
+
+    {
+        MainPanel project = new MainPanel();
+        project.createMainPanel();
+        project.setLocation(50, 50);
+        project.setVisible(true);
+        project.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
